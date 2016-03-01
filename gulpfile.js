844dba553e5aac475742d9b0ec6941f4fcb8f818
vasergen <vasergen@gmail.com>
2016-03-01 22:27:10 +0100

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,9 @@ gulp.task('jasmine', () => {
     .pipe(jasmine())
 })
 
-//dont work yet
 gulp.task('karma', function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
-  }, function() {
-    done()
-  }).start()
+  }, done).start()
 })

bbd9233f1642731abdc33fa7ac0f115dc4fa0270
vasergen <vasergen@gmail.com>
2016-03-02 17:08:09 +0100

    improving digest implementation

--- src/scope.js
+++ src/scope.js
@@ -2,35 +2,63 @@
 
 function Scope() {
   this.$$watchers = []
+  this.$$lastDirtyWatch = null
 }
 
 function initWhatchVal() {}
 
-Scope.prototype.$watch = function (whatchFn, listenerFn) {
+Scope.prototype.$$areEqual = function(newVal, oldVal, valueEq) {
+  if(valueEq)
+    return _.isEqual(newVal, oldVal)
+
+  return newVal === oldVal
+}
+
+Scope.prototype.$watch = function (whatchFn, listenerFn, valueEq) {
   let watcher = {
     whatchFn,
+    valueEq: !!valueEq,
     listenerFn: listenerFn || function() {},
     last: initWhatchVal
   }
 
   this.$$watchers.push(watcher)
+  this.$$lastDirtyWatch = null
 }
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$digestOnce = function () {
   let self = this
   let newVal
   let oldVal
+  let dirty
 
-  this.$$watchers.forEach((watcher) => {
+  _.forEach(this.$$watchers, (watcher) => {
     newVal = watcher.whatchFn(self)
     oldVal = watcher.last
-    if(newVal !== oldVal) {
-      watcher.last = newVal
+
+    if(!self.$$areEqual(newVal,oldVal, watcher.valueEq)) {
+      this.$$lastDirtyWatch = watcher
+      dirty = true
+      watcher.last = watcher.valueEq ? _.cloneDeep(newVal) : newVal
       watcher.listenerFn(
         newVal,
         (oldVal === initWhatchVal ? newVal : oldVal),
         self
       )
+    } else if (this.$$lastDirtyWatch == watcher) {
+      return false
     }
   })
+  return dirty
+}
+
+Scope.prototype.$digest = function() {
+  let dirty
+  let ttl = 10
+  this.$$lastDirtyWatch = null
+
+  do {
+    dirty = this.$digestOnce()
+    if(dirty && (!--ttl) ) throw "Maximum digest iteration reached"
+  } while (dirty)
 }
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -96,5 +96,123 @@ describe('Scope', () => {
       expect(watcherFn).toHaveBeenCalled()
     })
 
+    it('trigger chained watchers in the same digest', () => {
+      scope.name = 'Bob'
+
+      let watchName = (scope) => {
+        return scope.name
+      }
+      let changeNameUpper = (newVal, oldVal, scope) => {
+        if(newVal) {
+            scope.upperName = newVal.toUpperCase()
+        }
+      }
+      let watchUpperName = (scope) => {
+        return scope.upperName
+      }
+      let changeInitial = (newVal, oldVal, scope) => {
+        if(newVal) {
+          scope.initial = newVal.substring(0,1).concat('.')
+        }
+      }
+
+      scope.$watch(watchUpperName, changeInitial)
+      scope.$watch(watchName, changeNameUpper)
+
+      scope.$digest()
+      expect(scope.upperName).toBe('BOB')
+      expect(scope.initial).toBe('B.')
+
+      scope.name = 'John'
+      scope.$digest()
+      expect(scope.initial).toBe('J.')
+    })
+
+    it('check how many times digestOnce is called', () => {
+      let watcher1 = () => {}
+      spyOn(scope, '$digestOnce')
+
+      scope.$watch(watcher1)
+      scope.$digest()
+
+      expect(scope.$digestOnce).toHaveBeenCalled()
+    })
+
+    it('gives up on watchers after 10 iteration', () => {
+      scope.counterA = 0
+      scope.counterB = 0
+
+      let watcherA = (scope) => scope.counterA
+      let watcherB = (scope) => scope.counterB
+      let incA = (newVal, oldVal, scope) => scope.counterA++
+      let incB = (newVal, oldVal, scope) => scope.counterB++
+
+      scope.$watch(watcherA, incB)
+      scope.$watch(watcherB, incA)
+
+      expect(() => {scope.$digest()}).toThrow()
+    })
+
+    it('ends the digest when the last watch is clean', () => {
+      scope.arr = _.range(100)
+      let watchExecution = 0
+
+      scope.arr.forEach((value, index) => {
+        scope.$watch((scope) => {
+          watchExecution++
+          return scope.arr[index]
+        }, () => {
+
+        })
+      })
+
+      scope.$digest()
+      expect(watchExecution).toBe(200)
+
+      scope.arr[0] = -1
+      scope.$digest()
+
+      expect(watchExecution).toBe(301)
+    })
+
+    it('doesnt not end digest when new watcher added', () => {
+      scope.propA = 'a'
+      scope.counter = 0
+
+      let watcherA = (scope) => scope.propA
+      let incCounter = (newVal, oldVal, scope) => scope.counter++
+
+      scope.$watch(watcherA, (newVal, oldVal, scope) => {
+        scope.$watch(watcherA, incCounter)
+      })
+
+      scope.$digest()
+
+      expect(scope.counter).toBe(1)
+    })
+
+    it('test $$areEqual', () => {
+      expect(scope.$$areEqual(1,1)).toBe(true)
+      expect(scope.$$areEqual(1, '1')).toBe(false)
+      expect(scope.$$areEqual([1,2,3], [1,2,3], true)).toBe(true)
+      expect(scope.$$areEqual([1,2,3], [1,2,3])).toBe(false)
+    })
+
+    it('compares based on value if enabled', () => {
+      scope.propA = [1,2,3]
+      scope.counter = 0
+
+      let watcherA = (scope) => scope.propA
+      let incCounter = (newVal, oldVal, scope) => scope.counter++
+
+      scope.$watch(watcherA, incCounter, true)
+      scope.$digest()
+      expect(scope.counter).toBe(1)
+
+      scope.propA.push(4)
+      scope.$digest()
+      expect(scope.counter).toBe(2)
+    })
+
   })
 })
